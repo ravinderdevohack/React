8c01ffcf750458911c1658103fa78325ec142b07
ravinder <ravinderchauhan@gmail.com>
2025-12-28 19:52:05 +0530

    initial commit

---
+++ 01basicreact/src/main.jsx
@@ -0,0 +1,9 @@
+import ReactDom from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+const root = ReactDom.createRoot(document.getElementById('root'))
+
+root.render(
+  <App/>
+)
