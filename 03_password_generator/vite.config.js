eed4b58b143ce475969bbc56d3a33134b1f447dc
RAVINDER CHAUHAN <94591337+ravinderdevohack@users.noreply.github.com>
2025-12-30 17:16:51 +0530

    Set base path for Vite configuration

--- 03_password_generator/vite.config.js
+++ 03_password_generator/vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/03_password_generator/',
   plugins: [
     react(),
     tailwindcss() 
