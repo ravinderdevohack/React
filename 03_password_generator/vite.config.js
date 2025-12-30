a17a577a006bc0257ee5f2292415a86041fc26ef
RAVINDER CHAUHAN <94591337+ravinderdevohack@users.noreply.github.com>
2025-12-30 17:37:59 +0530

    Remove base path from Vite configuration

--- 03_password_generator/vite.config.js
+++ 03_password_generator/vite.config.js
@@ -4,7 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/03_password_generator/',
   plugins: [
     react(),
     tailwindcss() 
